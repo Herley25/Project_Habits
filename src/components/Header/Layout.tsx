93e2aa9f67de1010ae744cf0913629679ddb8a45
Herley <herleyoliveira19@gmail.com>
2023-03-07 15:10:10 -0300

    create navigation

--- src/components/Header/Layout.tsx
+++ src/components/Header/Layout.tsx
@@ -8,6 +8,9 @@ import colors from 'tailwindcss/colors';
 import {
     Feather,
 } from '@expo/vector-icons';
+import {
+    useNavigation,
+} from '@react-navigation/native';
 
 import Logo from '../../assets/logo.svg';
 import {
@@ -15,11 +18,13 @@ import {
 } from './styles';
 
 export const Layout = () => {
+  const { navigate } = useNavigation();
+
   return (
     <View style={styles.container}>
       <Logo />
 
-      <TouchableOpacity activeOpacity={0.7} style={styles.button}>
+      <TouchableOpacity activeOpacity={0.7} style={styles.button} onPress={() => navigate("new")}>
         <Feather name="plus" color={colors.violet[500]} size={20} />
 
         <Text style={styles.text}>Novo</Text>
